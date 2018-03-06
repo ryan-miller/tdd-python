933f90e9929e25b5993c58b281896c12f6fa3170
Ryan Miller <ryan_miller@vuzix.com>
2018-03-05 20:47:41 -0700

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -27,16 +27,16 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
         # She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
-        input.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists "1: Buy peacock feathers" as an item in a to-do list
-        input.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)
